afcdf8c9d835da99085b113bcff9bf402cf52347
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-11-18 18:44:35 +0530

    Create page layout

--- job-board-app/src/app/layout.tsx
+++ job-board-app/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
+import Header from "../app/Components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,11 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Link href="/">Job Board</Link>
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <main className="py-4 px-6 container mx-auto">
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   );
--- job-board-app/src/app/page.tsx
+++ job-board-app/src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "./Components/Hero";
+
 export default function Home() {
   return (
     <div>
-
+      <Hero />
     </div>
   );
 }
