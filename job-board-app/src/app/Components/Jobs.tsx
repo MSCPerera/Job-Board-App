2f50f019f00589808aca3d8ec0d16fc78e79fb94
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-11-25 21:53:52 +0530

    Create job row

--- job-board-app/src/app/Components/Jobs.tsx
+++ job-board-app/src/app/Components/Jobs.tsx
@@ -1,12 +1,21 @@
 export default function Jobs() {
     return (
-        <div className="bg-slate-400 p-8 py-4 rounded-xl">
+        <div className="bg-slate-200 p-8 py-4 rounded-xl">
             <div className="container">
-                <h2 className="p-4">Recent Jobs</h2>
+                <h2 className="p-2 font-bold mb-4">Recent Jobs</h2>
 
-                <div className="bg-white p-6 rounded-md shadow-sm">test job</div>
+                <div className="flex flex-col gap-4">
+                    <JobRow />
+                    <JobRow />
+                    <JobRow />
+                    <JobRow />
+                    <JobRow />
+                </div>
+                
 
             </div>
         </div>
     );
 }
+
+import JobRow from "./JobRow";
