f8ce1ff30895dda2142759f96d2353de1a86e84a
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-11-27 19:11:28 +0530

    Update JobRow.tsx

--- job-board-app/src/app/Components/JobRow.tsx
+++ job-board-app/src/app/Components/JobRow.tsx
@@ -6,7 +6,7 @@ export default function JobRow() {
         <>
             <div className="bg-white p-4 rounded-lg shadow-sm flex gap-4 relative">
                 <div className="absolute cursor-pointer top-4 right-4">
-                    <FontAwesomeIcon className="size-4 text-gray-500" icon={faHeart} />
+                    <FontAwesomeIcon className="size-4 text-gray-300" icon={faHeart} />
                 </div>
                 <div className="flex grow gap-4">
                     <div className="content-center">
