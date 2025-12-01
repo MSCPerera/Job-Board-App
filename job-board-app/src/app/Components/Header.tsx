e1db1f408bad62c42486cd786785a016d6b1e626
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-12-02 00:42:15 +0530

    Update user authentication

--- job-board-app/src/app/Components/Header.tsx
+++ job-board-app/src/app/Components/Header.tsx
@@ -1,13 +1,32 @@
 import Link from "next/link";
+import { getSignInUrl, getSignUpUrl, withAuth, signOut } from "@workos-inc/authkit-nextjs";
 
-export default function Header() {
+export default async function Header() {
+  const { user } = await withAuth();
+  
+  const signInUrl = await getSignInUrl();
+  const signOutUrl = await getSignUpUrl();
   return (
     <header>
       <div className="container flex items-center justify-between mx-auto my-4">
         <Link href={'/'} className="font-bold text-xl">Job Board</Link>
-        <nav className="flex gap-2 *:py-2 *:px-4 *:rounded-lg">
-          <Link className="bg-gray-200" href={'/login'}>Login</Link>
-          <Link className="bg-blue-600 text-white" href={'/new-listing'}>Post a job</Link>
+        <nav className="flex gap-2">
+          {!user && (
+            <Link className="bg-gray-200 rounded-md py-2 px-4" href={signInUrl}>
+              Login
+            </Link>
+          )}
+          {user && (
+            <form action={async () => {
+              'use server';
+              await signOut();
+            }}>
+              <button type="submit" className="rounded-md bg-gray-200 py-2 px-4">
+                Logout
+              </button>
+            </form>
+          )}
+          <Link className="rounded-md py-2 px-4 bg-blue-600 text-white" href={'/new-listing'}>Post a job</Link>
         </nav>
       </div>
     </header>
