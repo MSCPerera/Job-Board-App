485375573421722b195a6ba2433a2d2830b133f8
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-12-01 23:37:41 +0530

    Create user authentication

--- job-board-app/src/app/page.tsx
+++ job-board-app/src/app/page.tsx
@@ -1,7 +1,20 @@
 import Hero from "./Components/Hero";
 import Jobs from "./Components/Jobs";
+import Link from 'next/link';
+import {
+  getSignUpUrl,
+  withAuth,
+  signOut,
+} from '@workos-inc/authkit-nextjs';
 
-export default function Home() {
+export default async function Home() {
+  
+  // Retrieves the user from the session or returns `null` if no user is signed in
+  const { user } = await withAuth();
+
+  // Get the URL to redirect the user to AuthKit to sign up
+  const signUpUrl = await getSignUpUrl();
+  
   return (
     <>
       <Hero />
