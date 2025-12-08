ca1b75b7b6e9b2e8e23ee92aa2959187a1c7d00e
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-12-08 19:37:04 +0530

    Update job board

---
+++ job-board-app/src/app/new-listing/page.tsx
@@ -0,0 +1,8 @@
+import { getUser } from "@workos-inc/authkit-nextjs";
+
+export default async function NewListingPage() {
+    const { user } = await getUser()
+    return (
+        <div>new listing</div>
+    );
+}
